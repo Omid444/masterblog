849d02f6d776f47f1aed0e2670b5d0f21081cbd3
Omid444 <86495623+Omid444@users.noreply.github.com>
2025-06-12 17:09:18 +0200

    style.css created add.html and index.html created. in app.py endpoit for homepage and add book created

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+blog_posts = [
+    {'id': 1, 'author': 'John Doe', 'title': 'First Post', 'content': 'This is my first post.'},
+    {'id': 2, 'author': 'Jane Doe', 'title': 'Second Post', 'content': 'This is another post.'},
+    # More blog posts can go here...
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # Add the code that handles adding a new blog
+        title = request.form.get('title','')
+        author = request.form.get('author','')
+        content = request.form.get('content','')
+        blog_posts.append({'id': len(blog_posts)+1, 'author': author, 'title': title, 'content': content})
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
+
 
 
 if __name__ == '__main__':
